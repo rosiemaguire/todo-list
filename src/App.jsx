0264645ccb0d06e1201d5e1659ec5b876d72dbc2
rosiemaguire <rosemarymaguire19@yahoo.com.au>
2023-08-30 14:09:42 +1000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import TodoItem from './components/TodoItem';
+import TodoForm from './components/TodoForm';
+import './App.css'
+
+function App() {
+
+  const [todos, setTodos] = useState([
+    {
+      text: "Learn about React",
+      isCompleted: false,
+    },{
+      text: "Meet a friend for lunch",
+      isCompleted: false,
+    },{
+      text: "Build a really cool todo app",
+      isCompleted: false,
+    }
+  ]);
+
+  const addTodo = (text) => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  }
+
+  const completeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index,1);
+    setTodos(newTodos);
+  }
+
+  return (
+    <div className='app'>
+      <h1 className='todo-list'>My todo list</h1>
+      {todos.map((todo, index) => (
+        <TodoItem todo={todo} key={index} index={index} completeTodo={completeTodo} removeTodo={removeTodo} />
+      ))}
+      <TodoForm addTodo={addTodo}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TodoItem/index.jsx
@@ -0,0 +1,16 @@
+import './style.css'
+
+function TodoItem(props) {
+  return (
+    <div className={`todo ${props.todo.isCompleted ? "complete" : ""}`}>
+      {props.todo.text}
+      <div>
+        <button onClick={() =>
+          props.completeTodo(props.index)}>Complete</button>
+        <button onClick={()=>props.removeTodo(props.index)}>x</button>
+      </div>
+    </div>
+    );
+}
+
+export default TodoItem;
